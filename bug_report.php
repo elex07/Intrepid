7f3a00aa11e7894f16ad512ac2cfe6258c4be1dc
Pavi <root@snt.com>
2014-07-07 16:32:00 +0530

    abc

--- bug_report.php
+++ bug_report.php
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with MantisBT.  If not, see <http://www.gnu.org/licenses/>.
 
-	/**
+	/*
 	 * This page stores the reported bug
 	 *
 	 * @package MantisBT
